fce4fc7d7935ce26bde314a5ba3f4cdf0a729c03
rydnr <github@acm-sl.org>
2024-10-28 18:59:15 +0100

    Updated nixpkgs

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
   description = "Artifact for pythoneda-shared-pythonlang/banner";
   inputs = rec {
     flake-utils = { url = "github:numtide/flake-utils/v1.0.0"; };
-    nixos = { url = "github:NixOS/nixpkgs/23.11"; };
+    nixos = { url = "github:NixOS/nixpkgs/24.05"; };
     pythoneda-external-artf-flakeutils = {
       inputs.flake-utils.follows = "flake-utils";
       inputs.nixos.follows = "nixos";
