e0e6a07fedc2d511b2e6e12f006b269d3637d650
rydnr <github@acm-sl.org>
2024-12-12 11:19:50 +0100

    Minor change

--- flake.nix
+++ flake.nix
@@ -1,19 +1,21 @@
-# banner/flake.nix
+# flake.nix
 #
-# This file defines a nix flake for banner-0.0.0.
-#
-# GNU GENERAL PUBLIC LICENSE
-# Version 3, 29 June 2007
-#
-# Copyright (C) 2024-today rydnr https://github.com/pythoneda-shared-pythonlang-artf/banner
+# Copyright (C) 2024-today rydnr's pythoneda-shared-pythonlang-artf-def/banner
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 {
-  description = "Artifact for pythoneda-shared-pythonlang/banner";
+  description = "Nix flake for pythoneda-shared-pythonlang-artf/banner";
   inputs = rec {
     flake-utils = { url = "github:numtide/flake-utils/v1.0.0"; };
     nixpkgs = { url = "github:NixOS/nixpkgs/24.05"; };
